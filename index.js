ac5f02a5a119033372a0e21b388dcfbd26c19bed
fisker <lionkay@gmail.com>
2019-03-27 14:05:18 +0800

    feat: standalone vue config

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * dev notice:
+ * don't use @vue/eslint-config-prettier
+ * eslint:recommended extend in this config breaks airbnb
+ * check this https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/eslint-config-prettier/index.js
+ */
+
+module.exports = {
+  root: true,
+  plugins: ['vue'],
+  extends: [require.resolve('@xwtec/eslint-config'), 'plugin:vue/essential'],
+  rules: {
+    // allow unused vars
+    'vue/no-unused-vars': 'off',
+
+    // allow unused components
+    'vue/no-unused-components': 'off',
+
+    'vue/no-use-v-if-with-v-for': 'off',
+
+    'vue/require-component-is': 'warn',
+
+    'vue/require-render-return': 'warn',
+
+    'vue/require-v-for-key': 'off',
+
+    'vue/require-valid-default-prop': 'warn',
+
+    'vue/return-in-computed-property': 'warn',
+
+    'vue/use-v-on-exact': 'off',
+
+    'vue/valid-template-root': 'warn',
+  },
+}
---
+++ scripts/rules.js
@@ -0,0 +1,41 @@
+import {join} from 'path'
+import {readFileSync} from 'fs'
+import printer from './shared/markdown-rules-printer'
+import writeFile from './shared/write-file'
+import getRules from './shared/get-rules'
+import parseRuleId from './shared/parse-rule-id'
+import sortRuleId from './shared/sort-rule-id'
+import config from '..'
+
+const rules = getRules(config)
+const data = Object.keys(rules)
+  .sort(sortRuleId)
+  .map(ruleId => ({
+    ...parseRuleId(ruleId),
+    ...rules[ruleId],
+  }))
+  .filter(({prefix}) => prefix === 'vue')
+
+const RULE_START_MARK = '<!-- rules start -->'
+const RULE_END_MARK = '<!-- rules end -->'
+
+const warnData = data.filter(({value}) => value === 'warn')
+const errorData = data.filter(({value}) => value === 'error')
+
+const content = [
+  RULE_START_MARK,
+  "<!-- AUTO GENERATED CONTENT, DON'T EDIT -->",
+  '### Error',
+  printer(errorData),
+  '### Warn',
+  printer(warnData),
+  RULE_END_MARK,
+].join('\n\n')
+
+const readmeFile = join(__dirname, `../readme.md`)
+const readme = readFileSync(readmeFile, 'utf8').replace(
+  new RegExp(`${RULE_START_MARK}[\\s\\S]*?${RULE_END_MARK}`, 'm'),
+  content
+)
+
+writeFile(readmeFile, readme)
