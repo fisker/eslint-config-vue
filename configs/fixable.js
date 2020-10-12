ca4d508fe493ac8266768c394668ffb8454a03d1
fisker <lionkay@gmail.com>
2020-10-12 14:31:31 +0800

    feat: enable all fixable rules

--- configs/fixable.js
+++ configs/fixable.js
@@ -1,47 +1,22 @@
-// fixable
-// https://vuejs.github.io/eslint-plugin-vue/rules/
-// [...$$('td')].filter(({textContent}) => textContent.includes('🔧')).map(el => el.parentNode.firstChild.textContent.trim().replace(/^vue\//, ''))
+// Fixable rules
 
-const ruleIDs = [
-  'no-shared-component-data',
-  'require-prop-type-constructor',
-  'attribute-hyphenation',
-  'html-closing-bracket-newline',
-  'html-closing-bracket-spacing',
-  'html-end-tags',
-  'html-indent',
-  'html-quotes',
-  'html-self-closing',
-  'max-attributes-per-line',
-  'multiline-html-element-content-newline',
-  'mustache-interpolation-spacing',
-  'name-property-casing',
-  'no-deprecated-slot-attribute',
-  'no-deprecated-slot-scope-attribute',
-  'no-multi-spaces',
-  'no-spaces-around-equal-signs-in-attribute',
-  'prop-name-casing',
-  'singleline-html-element-content-newline',
-  'static-class-names-order',
-  'v-bind-style',
-  'v-on-style',
-  'attributes-order',
-  'order-in-components',
-  'component-name-in-template-casing',
-  'component-definition-name-casing',
-  'script-indent',
-];
+const {rules} = require('eslint-plugin-vue');
 
-const rules = Object.fromEntries
-  ? Object.fromEntries(ruleIDs.map((ruleId) => [`vue/${ruleId}`, 'warn']))
-  : ruleIDs.reduce((rules, ruleId) => {
-      rules[`vue/${ruleId}`] = 'warn';
-      return rules;
-    }, {});
+const fixableRules = {};
+for (const [
+  ruleId,
+  {
+    meta: {fixable},
+  },
+] of Object.entries(rules)) {
+  if (fixable) {
+    fixableRules[`vue/${ruleId}`] = 'warn';
+  }
+}
 
 module.exports = {
   rules: {
-    ...rules,
+    ...fixableRules,
 
     // https://eslint.vuejs.org/rules/component-name-in-template-casing.html
     'vue/component-name-in-template-casing': ['warn', 'kebab-case'],
